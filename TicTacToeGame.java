54ad661e086b5907eefc0847f3ca545139fb1db9
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-11-23 09:03:21 +0530

    [Saiprasad]Added: GameBoard in TicTacToe Class

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -4,5 +4,17 @@ public class TicTacToeGame {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Tic Tac Toe");
+		TicTacToeGame TTTG = new TicTacToeGame();
+		TTTG.GameBoard();
 	}
+	
+	public void GameBoard() {
+		char [] tictactoeboard = new char[10]; 
+		for(int i=0; i<tictactoeboard.length;i++)
+		{
+			tictactoeboard[i]= ' ';
+		}
+		System.out.println("Created a Game Board of size "+ tictactoeboard.length);
+	}
+
 }
