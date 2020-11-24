f0e2f892ec959dd64dbe3649048805c606283336
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-11-24 09:49:25 +0530

    [Saiprasad] Added: Ability to choose letter for player

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -5,12 +5,12 @@ public class TicTacToeGame {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Tic Tac Toe");
-		TicTacToeGame TTTG = new TicTacToeGame();
-		TTTG.GameBoard();
-		TTTG.AbilityToChoosePlayer();
+		TicTacToeGame game = new TicTacToeGame();
+		game.createBoard();
+		game.abilityToChoosePlayer();
 	}
-	
-	public void GameBoard() {
+	// Creating the empty board 
+	public void createBoard() {
 		char [] tictactoeboard = new char[10]; 
 		for(int i=0; i<tictactoeboard.length;i++)
 		{
@@ -18,8 +18,8 @@ public class TicTacToeGame {
 		}
 		System.out.println("Created a Game Board of size "+ tictactoeboard.length);
 	}
-	
-	public void AbilityToChoosePlayer() {
+	// Here player will choose the letter with whom he wants to play
+	public void abilityToChoosePlayer() {
 		System.out.println("Which letter you want to choose:\n 1.X (UpperCase)\n 2. O (UpperCase)");
 		char player = sc.next().charAt(0);
 		System.out.println("letter choosen by player: "+player);
