d4c1db2bf7995a3c5c919c2d6da3c340dfa688ae
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-11-24 11:20:15 +0530

    [Saiprasad] Added: Ability to move while checking position is empty

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -2,61 +2,68 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
 	static Scanner sc = new Scanner(System.in);
-		 
-	static char [] tictactoeboard = new char[10]; 		
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Tic Tac Toe");
-		for(int i=0; i<tictactoeboard.length;i++)
-		{
-			tictactoeboard[i]= ' ';
-		}
-		TicTacToeGame TTTG = new TicTacToeGame();
-		TTTG.AbilityToChoosePlayer();
-		TTTG.AbilityToMove(tictactoeboard);
-		TTTG.AbilityToMove(tictactoeboard);
-		TTTG.DisplayBoard(tictactoeboard);
-	}
-	
-	
-	public void AbilityToChoosePlayer() {
-		System.out.println("Which letter you want to choose:\n 1.X (UpperCase)\n 2. O (UpperCase)");
-		char player = sc.next().charAt(0);
-		System.out.println("letter choosen by player: "+player);
-		char computer;
-		if (player == 'X') {
+
+		char[] board = createBoard();
+		char player = abilityToChoosePlayer();
+		char computer = ' ';
+		if (player == 'X' || player == 'x') {
 			computer = 'O';
-		}
-		else
-		{
+		} else if (player == 'O' || player == 'o') {
 			computer = 'X';
 		}
-		System.out.println("letter choosen by computer: "+computer);
+		System.out.println("letter choosen by player: " + player + " computer: " + computer);
+		displayBoard(board);
+		abilityToMove(board);
 	}
-	
-	public void DisplayBoard(char [] tictactoeboard) {
-		System.out.println(tictactoeboard[1]+ " | "+tictactoeboard[2]+ " | "+tictactoeboard[3]);
-		System.out.println("___________");
-		System.out.println(tictactoeboard[4]+ " | "+tictactoeboard[5]+ " | "+tictactoeboard[6]);
-		System.out.println("___________");
-		System.out.println(tictactoeboard[7]+ " | "+tictactoeboard[8]+ " | "+tictactoeboard[9]);
+
+	// Creating the empty board
+	private static char[] createBoard() {
+		char[] tictactoeboard = new char[10];
+		for (int i = 0; i < tictactoeboard.length; i++) {
+			tictactoeboard[i] = ' ';
+		}
+		System.out.println("Created a Game Board of size " + tictactoeboard.length);
+		return tictactoeboard;
 	}
-	
-	public void AbilityToMove(char [] tictactoeboard) {
+
+	// Here player will choose the letter with whom he wants to play
+	private static char abilityToChoosePlayer() {
+		System.out.println("Which letter you want to choose: X  Or O ");
+		char player = sc.next().charAt(0);
+
+		return player;
+	}
+
+	// Displaying the board
+	private static void displayBoard(char[] tictactoeboard) {
+		System.out.println(tictactoeboard[1] + " | " + tictactoeboard[2] + " | " + tictactoeboard[3]);
+		System.out.println("___________");
+		System.out.println(tictactoeboard[4] + " | " + tictactoeboard[5] + " | " + tictactoeboard[6]);
+		System.out.println("___________");
+		System.out.println(tictactoeboard[7] + " | " + tictactoeboard[8] + " | " + tictactoeboard[9]);
+	}
+
+	// Ability to move anywhere in board while checking the space is empty
+	private static void abilityToMove(char[] tictactoeboard) {
 		System.out.println("Enter the position where you want to move from(1 to 9):");
 		int position = sc.nextInt();
 		if (position > 9 || position < 1) {
-			System.out.println("please enter valid position");	
-		}
-		else {
-			if(tictactoeboard[position] == ' ')
+			System.out.println("please enter valid position");
+			abilityToMove(tictactoeboard);
+		} else {
+			if (tictactoeboard[position] == ' ') {
 				tictactoeboard[position] = 'X';
-			else {
+				displayBoard(tictactoeboard);
+				abilityToMove(tictactoeboard);
+			} else {
 				System.out.println("Enter Valid position\n");
-				AbilityToMove(tictactoeboard);
+				abilityToMove(tictactoeboard);
 			}
 		}
-		
 	}
 
 }
